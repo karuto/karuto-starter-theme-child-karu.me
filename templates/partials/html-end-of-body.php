3c0a8e9554e9107a62db5a3f5ab73de6ddde3043
Vincent Zhang <iamkaruto@gmail.com>
2020-03-14 21:46:43 -0700

    Init migration

---
+++ templates/partials/html-end-of-body.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This is the code that runs right before the end of body.
+ * For example, put your third-party tracking scripts here.
+ *
+ * @package WordPress
+ * @subpackage karuto_starter_theme
+ * @since added by Vincent Zhang
+ */
+
+?>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55145791-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-55145791-2');
+</script>
