4d1f4288eace0e911a29f7b9cc436a5a59aa4bca
Vincent Zhang <iamkaruto@gmail.com>
2020-03-14 21:53:02 -0700

    Updated content

--- templates/partials/head.php
+++ templates/partials/head.php
@@ -17,8 +17,8 @@
 
 	<!-- TODO: this section below is specific to the site. Make it adaptable later. -->
   <link rel="copyright" href="/copyright">
-  <meta name="keywords" content="">
-  <meta name="description" content="">
+  <meta name="keywords" content="Karuto, self-improvement, essay, manga, MangaTalk, anime, acg, comics, review, 漫画, 漫画手札, 漫言, 动漫, 动画, 漫评, 影评, 动画评论, 漫画评论, 独立博客">
+  <meta name="description" content="Karuto 的独立博客。你好。我是一个平凡而认真的人，试着在世界上留下一点价值。漫言 MangaTalk 创始人，动新、漫友漫评作者。">
   <link href="https://fonts.googleapis.com/css?family=Noto+Serif|Roboto:400,700|" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
